d394a353b9b6b3ee027b47da4e7d673f869ebcd5
maksym.dieliukin <maksym.dieliukin@sharecat.com>
2021-09-06 01:57:19 +0300

    Renamed components

---
+++ app/src/ProductItemDetails.tsx
@@ -0,0 +1,47 @@
+import { DeleteButton } from "./DeleteButton";
+//TODO: rename to ItemDetails or ProductItemDetails
+interface IProductItemDetails {
+    item: any;
+    getClassName: (field: string) => string;
+    onDeleteClick: (id: string) => void;
+    sortedFeatures: string[];
+}
+
+export const ProductItemDetails = ({ item, getClassName, onDeleteClick, sortedFeatures }: IProductItemDetails) => {
+    //TODO: add try catch block
+    const badgesString = item['badges'];
+    const badgeUrls = badgesString.split('|');//TODO: check whether it makes sense to sanitize JSON input
+
+    return <li>
+        <DeleteButton onDeleteClick={() => { onDeleteClick(item['sku']) }}>
+            Delete
+        </DeleteButton>
+        <div>
+            <div>
+                <img src={item['productImage']} />
+            </div>
+            <div>
+                {item['name']}
+            </div>
+            <div>
+                {item['salePrice']}
+            </div>
+        </div>
+        <ul className='badgesWrap'>
+            {badgeUrls.map((badgeUrl: string, index: number) => {
+                return <li key={index}>
+                    <img src={badgeUrl} />
+                </li>
+            })}
+        </ul>
+        <div>
+            {sortedFeatures
+            .filter( (feature: string) => (feature !== 'badges') )
+            .map((feature: string) => {
+                return <div className={getClassName(feature)}>
+                    {item[feature]}
+                </div>;
+            })}
+        </div>
+    </li>;
+};
