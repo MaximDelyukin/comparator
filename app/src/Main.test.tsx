add7a344afad19cd39e40de4baddb2d0bdc2f52a
maksym.dieliukin <maksym.dieliukin@sharecat.com>
2021-09-09 03:48:52 +0300

    Fixed test to be closer to how end user uses the application.

--- app/src/Main.test.tsx
+++ app/src/Main.test.tsx
@@ -6,65 +6,7 @@ import { CONSTANTS } from './utils';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
-const defaultProducts = [
-    {
-        "Toepassing": "Voedsel en dranken",
-        "salePrice": "0.66",
-        "manufacturerName": "",
-        "Hardheid": "70",
-        "grossPrice": "0.80",
-        "BUP_UOM": "",
-        "Artikelnummer": "11545A",
-        "stepQuantity": "5",
-        "BUP_Value": "",
-        "badges": "https://eriksdigitalcdn.azureedge.net/shop/thumb40/hlr-system/egt/pubnl/pim_icons/rohs-icon-nl.jpg|https://eriksdigitalcdn.azureedge.net/shop/thumb40/hlr-system/egt/pubnl/pim_icons/rohs-icon-nl.jpg|https://eriksdigitalcdn.azureedge.net/shop/thumb40/hlr-system/egt/pubnl/pim_icons/omega-slang-icon-nl.jpg|https://eriksdigitalcdn.azureedge.net/shop/thumb40/hlr-system/egt/pubnl/pim_icons/o-t-symbol-icon-nl.jpg",
-        "uom": "Stuk",
-        "Kleur": "Zwart",
-        "productImage": "https://eriksdigitalcdn.azureedge.net/shop/thumb/hlr-system/webshop/lag/lag-product-images/lag25/lag00.png",
-        "Temperatuurgebied": "van  -50  tot  150",
-        "BUP_Conversion": "",
-        "minQuantity": "5",
-        "manufacturerImage": "",
-        "name": "O-ring EPDM 70 shore - Voedsel (Binnen Ø=1.78 Snoer Ø=1.78; AS568- 004)",
-        "Materiaal": "EPDM",
-        "sku": "11545A",
-        "Snoerdikte": "1.78",
-        "Inwendige diameter": "1.22",
-        "Maat volgens AS568": "200",
-        "listPrice": "0.66",
-        "channel": "nl_NL",
-        "display": true,
-        "atp": {}
-    },
-    {
-        "Toepassing": "Voedsel en dranken",
-        "salePrice": "1.36",
-        "manufacturerName": "",
-        "Hardheid": "70",
-        "grossPrice": "1.65",
-        "BUP_UOM": "",
-        "Artikelnummer": "115E1A",
-        "stepQuantity": "5",
-        "BUP_Value": "",
-        "badges": "https://eriksdigitalcdn.azureedge.net/shop/thumb40/hlr-system/egt/pubnl/pim_icons/rohs-icon-nl.jpg|https://eriksdigitalcdn.azureedge.net/shop/thumb40/hlr-system/egt/pubnl/pim_icons/omega-slang-icon-nl.jpg|https://eriksdigitalcdn.azureedge.net/shop/thumb40/hlr-system/egt/pubnl/pim_icons/o-t-symbol-icon-nl.jpg",
-        "uom": "Stuk",
-        "Kleur": "Zwart",
-        "productImage": "https://eriksdigitalcdn.azureedge.net/shop/thumb/hlr-system/blickle/internet images/lrxa-po_76g-fi_754998.jpg",
-        "Temperatuurgebied": "van  -50  tot  150",
-        "BUP_Conversion": "",
-        "minQuantity": "5",
-        "manufacturerImage": "",
-        "name": "O-ring EPDM 70 shore - Voedsel (Binnen Ø=2.06 Snoer Ø=2.62; AS568- 103)",
-        "Materiaal": "EPDM",
-        "sku": "115E1A",
-        "Snoerdikte": "2.62",
-        "Inwendige diameter": "2.06",
-        "Maat volgens AS568": "100",
-        "listPrice": "1.36",
-        "channel": "nl_NL",
-        "display": true,
-        "atp": {}
-    },
+const productsWithOneDifferentFeature = [
     {
         "Toepassing": "Voedsel en dranken",
         "salePrice": "0.66",
@@ -83,7 +25,7 @@ const defaultProducts = [
         "BUP_Conversion": "",
         "minQuantity": "5",
         "manufacturerImage": "",
-        "name": "O-ring EPDM 70 shore - Voedsel (Binnen Ø=2.9 Snoer Ø=1.78; AS568- 006)",
+        "name": "O-ring EPDM 70 shore - Voedsel (Binnen Ø=2.5 Snoer Ø=1.78; AS568- 002)",
         "Materiaal": "EPDM",
         "sku": "115576",
         "Snoerdikte": "1.78",
@@ -93,10 +35,7 @@ const defaultProducts = [
         "channel": "nl_NL",
         "display": true,
         "atp": {}
-    }
-];
-
-const productsWithOneDifferentFeature = [
+    },
     {
         "Toepassing": "Voedsel en dranken",
         "salePrice": "0.66",
@@ -169,7 +108,7 @@ afterAll(() => server.close());
 afterEach(() => server.resetHandlers());
 
 
-test.only('allows user to view list of products selected for comparison', async () => {
+test('allows user to view list of products selected for comparison', async () => {
     render(<App />);
     const productName = productsWithOneDifferentFeature[0]['name'];
     const productItemDetails = await screen.findByText(productName, { selector: '.productTitleWrap b' });
@@ -177,7 +116,7 @@ test.only('allows user to view list of products selected for comparison', async 
     expect(productItemDetails).toBeVisible();
 });
 
-test.only('allows user to toggle visibility of selected products', async () => {
+test('allows user to toggle visibility of selected products', async () => {
     render(<App />);
     const productName = productsWithOneDifferentFeature[0]['name'];
     const checkbox = await screen.findByLabelText(productName);
@@ -185,7 +124,7 @@ test.only('allows user to toggle visibility of selected products', async () => {
     expect(checkbox).toBeVisible();
 });
 
-test.only('allows user to delete products available for comparison', async () => {
+test('allows user to delete products available for comparison', async () => {
     render(<App />);
 
     const deleteButton: any = await screen.findAllByTitle('Remove Product');
@@ -193,7 +132,7 @@ test.only('allows user to delete products available for comparison', async () =>
     expect(deleteButton[0]).toBeVisible();
 });
 
-test.only('the amount of product titles with checkboxes should match the amount of visible products', async () => {
+test('the amount of product titles with checkboxes should match the amount of visible products', async () => {
     render(<App />);
 
     const productNameFragment = /O-ring EPDM 70 shore/i;
@@ -207,7 +146,7 @@ test.only('the amount of product titles with checkboxes should match the amount 
 
 });
 
-test.only('toggling checkboxes should toggle the amount of product items visible for comparison', async () => {
+test('toggling checkboxes should toggle the amount of product items visible for comparison', async () => {
     render(<App />);
 
     const productNameFragment = /O-ring EPDM 70 shore/i;
@@ -230,53 +169,58 @@ test.only('toggling checkboxes should toggle the amount of product items visible
     expect(visibleProductsAfterSecondCheckboxClickAmount).toEqual(visibleProductsBeforeUserClicksCheckboxAmount);
 });
 
-test('user should see highlighted cells with features which are different among the products', () => {
+test('user should see highlighted cells with features which are different among the products', async () => {
     render(<App />);
 
     const differentFeature = 'Inwendige diameter';
     const differenceMarkClassName = 'markDifference';
 
-    const elementsWithDifferentFeatureValue = document.querySelectorAll(`[data-testid='${differentFeature}']`);
-
-    Array.from(elementsWithDifferentFeatureValue).every((el: any, i: number, arr: any[]) => el.className === differenceMarkClassName);
+    const elementsWithDifferentFeatureValue = await screen.findAllByTestId(differentFeature);
+    const elsArr = Array.from(elementsWithDifferentFeatureValue);
+    expect(elsArr.every((el: any, i: number, arr: any[]) => el.className === differenceMarkClassName)).toBe(true);
 });
 
-test('user should see no highlighted cells of the features which are the same among the products', () => {
+test('user should see no highlighted cells of the features which are the same among the products', async () => {
     render(<App />);
 
     const sameFeature = 'Hardheid';
     const noDifferenceMarkClassName = 'markNoDifference';
 
-    const elementsWithDifferentFeatureValue = document.querySelectorAll(`[data-testid='${sameFeature}']`);
-
-    Array.from(elementsWithDifferentFeatureValue).every((el: any, i: number, arr: any[]) => el.className === noDifferenceMarkClassName);
+    const elementsWithSameFeatureValue = await screen.findAllByTestId(sameFeature);
+    const elsArr = Array.from(elementsWithSameFeatureValue);
+    expect(elsArr.every((el: any, i: number, arr: any[]) => el.className === noDifferenceMarkClassName)).toBe(true);
 });
 
-test('integration', () => {
+test('integration', async () => {
     render(<App />);
 
-    const visibleProductsTogglerContainer = document.querySelector('.comparedItemsListWrap');
-    const checkBoxesBeforeDeleteClick = visibleProductsTogglerContainer!.querySelectorAll('input[type=checkbox]');
+    const productNameFragment = /O-ring EPDM 70 shore/i;
+    const checkBoxesBeforeDeleteClick = await screen.findAllByLabelText(productNameFragment, {selector: 'input[type=checkbox]'});
 
-    const productsDetailsContainer = document.querySelector('.featuresDetailedItemsListWrap');
+    const deleteButtons = await screen.findAllByTitle('Remove Product');
+    const deleteButton = deleteButtons[0];
 
-    const deleteButton = productsDetailsContainer?.querySelector('.buttonDelete');
-
-    const visibleProductsBeforeDeleteClick = productsDetailsContainer!.querySelectorAll('.productDetailsItem');
+    const visibleProductsBeforeDeleteClick = await screen.findAllByText(
+        productNameFragment,
+        { selector: '.productDetailsItem .productTitleWrap b' }
+    );
     const visibleProductsBeforeDeleteClickAmount = visibleProductsBeforeDeleteClick.length;
 
     const feature = 'Maat volgens AS568';
     const differenceMarkClassName = 'markDifference';
 
-    const elementsWithFeatureValueBeforeDeleteClick = document.querySelectorAll(`[data-testid='${feature}']`);
+    const elementsWithFeatureValueBeforeDeleteClick = await screen.findAllByTestId(feature);
     
     const isAllMarked = Array.from(elementsWithFeatureValueBeforeDeleteClick).every((el: any) => el.className === differenceMarkClassName);
     expect(isAllMarked).toBe(true);
 
-    user.click(deleteButton!);
-    const checkboxesAfterDeleteClick = visibleProductsTogglerContainer!.querySelectorAll('input[type=checkbox]');
+    user.click(deleteButton);
+    const checkboxesAfterDeleteClick = await screen.findAllByLabelText(productNameFragment, {selector: 'input[type=checkbox]'});
 
-    const visibleProductsAfterDeleteClick = productsDetailsContainer!.querySelectorAll('.productDetailsItem');
+    const visibleProductsAfterDeleteClick = await screen.findAllByText(
+        productNameFragment,
+        { selector: '.productDetailsItem .productTitleWrap b' }
+    );
     const visibleProductsAfterDeleteClickAmount = visibleProductsAfterDeleteClick.length;
 
     expect(checkboxesAfterDeleteClick.length).toBeLessThan(checkBoxesBeforeDeleteClick.length);
@@ -284,7 +228,7 @@ test('integration', () => {
 
     const noDifferenceMarkClassName = 'markNoDifference';
 
-    const elementsWithFeatureValueAfterDeleteClick = document.querySelectorAll(`[data-testid='${feature}']`);
+    const elementsWithFeatureValueAfterDeleteClick =  await screen.findAllByTestId(feature);
     const isAllNonMarked = Array.from(elementsWithFeatureValueAfterDeleteClick).every((el: any) => el.className === noDifferenceMarkClassName);
     expect(isAllNonMarked).toBe(true);
 });
